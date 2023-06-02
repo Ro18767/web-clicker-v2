f4164df244f30b3d21b060f40997317fb303aeeb
TimaR0 <trodoslavov@gmail.com>
2023-06-02 19:10:12 +0300

    быстрый фикс

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,26 +1,5 @@
 <script>
-  import Game from "$lib/Game/Game.svelte";
   import Menu from "$lib/Menu/Menu.svelte";
 </script>
 
 <Menu />
-
-<style>
-  .Game {
-    width: 100%;
-    height: 100%;
-    padding: 0;
-    margin: 0;
-    display: grid;
-    grid-template-areas:
-      "Balance LevelSelector"
-      "Store Enemy"
-      "GameStatistics Enemy"
-      "AudioSetting SaveSetting";
-    grid-template-columns: 50% 50%;
-    grid-template-rows: auto 1fr 1fr auto;
-
-    position: relative;
-  }
-
-</style>
